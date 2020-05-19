32e0eede61cc3e5a72adb9e4d3d4abb90ce652c4
chathurikaPerera <pererachathurika96@gmail.com>
2020-05-19 13:16:42 +0530

    assignment

--- scala/HelloWorld.scala
+++ scala/HelloWorld.scala
@@ -3,10 +3,10 @@ object HelloWorld
 {
    def main(ags:Array[String])
    {
-     println(convert(36));
+     println(convert(35));
      println(volume(5));
      println(price(60));
-     
+
    }
    //question 1
    def convert(x:Double)=x*1.8+32;
@@ -14,12 +14,12 @@ object HelloWorld
    def volume(r:Double)=(4/3.0)*math.Pi*(r*r*r);
    //question 3
    def price(y:Int) : Double = {
-   
+
      var discount:Double=24.95*0.6*y;
      var shipping : Double=3*50 + 0.75*(y-50);
      var sum : Double= discount+shipping;
      return sum;
-     
+
    }
-   
+
 }
